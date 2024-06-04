c5c1441912430da3cad41884b6b0495694e02bbb
davidecapone <davidecapone2304@gmail.com>
2024-06-04 08:39:40 +0200

    add the check_the_wrong_answer_is_incorrect test

--- app/src/test/java/project/study/app/QuestionUnitTest.java
+++ app/src/test/java/project/study/app/QuestionUnitTest.java
@@ -7,7 +7,7 @@ import static project.study.app.MultipleChoiceQuestion.Answer;
 
 public class QuestionUnitTest {
     @Test
-    public void check_the_answer_is_correct() {
+    public void check_the_correct_answer_is_correct() {
         // question text:
         String question = "Paris is the capital of France";
         // possible answer 1 (the correct one):
@@ -18,7 +18,19 @@ public class QuestionUnitTest {
         Answer[] answers = {answer1, answer2};
         MultipleChoiceQuestion multipleChoiceQuestion = new MultipleChoiceQuestion(question, answers);
 
-        // check if passing the correct answer the method returns isCorrect = true
+        // check if passing the correct answer the method returns isCorrect true
         assertEquals(multipleChoiceQuestion.isCorrect(answer1), true);
+    }
+
+    @Test
+    public void check_the_wrong_answer_is_incorrect() {
+        String question = "Rome is the capital of Italy";
+        Answer answer1 = new Answer("Yes", true);
+        Answer answer2 = new Answer("No", false);
+        Answer[] answers = {answer1, answer2};
+        MultipleChoiceQuestion mcq = new MultipleChoiceQuestion(question, answers);
+
+        // the answer2 should be incorrect:
+        assertEquals(mcq.isCorrect(answer2), false);
     }
 }
