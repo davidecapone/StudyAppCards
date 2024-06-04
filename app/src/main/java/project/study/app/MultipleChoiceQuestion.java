86b54743be249e4e238185997d3def71a148408f
5JDR <dellarovere.sandro@gmail.com>
2024-06-04 09:58:40 +0200

    Refactoring and contracts

--- app/src/main/java/project/study/app/MultipleChoiceQuestion.java
+++ app/src/main/java/project/study/app/MultipleChoiceQuestion.java
@@ -1,24 +1,56 @@
 package project.study.app;
 
+/**
+ * Represents a multiple choice question.
+ */
 public class MultipleChoiceQuestion {
+
+    // The question text.
     private String questionText;
+
+    // The possible answers.
     private Answer[] answers;
 
+    /**
+     * Constructor.
+     *
+     * @param questionText The question text.
+     * @param answers The possible answers.
+     */
     public MultipleChoiceQuestion(String questionText, Answer[] answers) {
         this.questionText = questionText;
         this.answers = answers;
     }
 
+    /**
+     * Gets the question text.
+     *
+     * @return The question text.
+     */
     public String getQuestionText() {
         return this.questionText;
     }
 
+    /**
+     * Sets the question text.
+     *
+     * @param questionText The question text.
+     */
     public void setQuestionText(String questionText) {
         this.questionText = questionText;
     }
 
     /**
-     * Checks if the input answer is correct.
+     * Gets the possible answers.
+     *
+     * @return The possible answers.
+     */
+    public Answer[] getAnswers(){
+        return this.answers;
+    }
+
+    /**
+     * Checks if the input answer is the correct one.
      *
      * @return true if the answer is correct, false otherwise.
      */
@@ -26,23 +58,70 @@ public class MultipleChoiceQuestion {
         return answer.getCorrectness();
     }
 
-    public Answer[] getAnswers(){
-        return this.answers;
-    }
-
+    /**
+     * Represents an answer to a multiple choice question.
+     */
     public static class Answer{
+
+        // The answer text.
         private String answerText;
+
+        // The correctness of the answer.
         private Boolean correctness;
 
+        /**
+         * Constructor.
+         *
+         * @param answerText The answer text.
+         * @param correctness The correctness of the answer.
+         */
         public Answer(String answerText, Boolean correctness){
             this.answerText = answerText;
             this.correctness = correctness;
         }
 
+        /**
+         * Gets the answer text.
+         *
+         * @return The answer text.
+         */
+        public String getAnswerText(){
+            return this.answerText;
+        }
+
+        /**
+         * Sets the answer text.
+         *
+         * @param answerText The answer text.
+         */
+        public void setAnswerText(String answerText){
+            this.answerText = answerText;
+        }
+
+        /**
+         * Gets the correctness of the answer.
+         *
+         * @return The correctness of the answer.
+         */
         public Boolean getCorrectness(){
             return this.correctness;
         }
 
+        /**
+         * Sets the correctness of the answer.
+         *
+         * @param correctness The correctness of the answer.
+         */
+        public void setCorrectness(Boolean correctness){
+            this.correctness = correctness;
+        }
+
+        /**
+         * Checks equality of two answer objects.
+         *
+         * @param obj The object to compare.
+         * @return true if the answers are equal, false otherwise.
+         */
         @Override
         public boolean equals(Object obj) {
             if (this == obj) {
@@ -53,18 +132,6 @@ public class MultipleChoiceQuestion {
             }
             Answer answer = (Answer) obj;
             return answerText.equals(answer.answerText) && correctness.equals(answer.correctness);
-        }
-
-        public String getAnswerText(){
-            return this.answerText;
-        }
-
-        public void setAnswerText(String answerText){
-            this.answerText = answerText;
-        }
-
-        public void setCorrectness(Boolean correctness){
-            this.correctness = correctness;
         }
     }
 }
