ecdfc6fc9b4e3f4d379c9f7e4449c33678b088ff
davidecapone <davidecapone2304@gmail.com>
2024-06-12 18:02:21 +0200

    add type converter to SudyAppDatabase

--- app/src/main/java/project/study/app/model/dao/StudyAppDatabase.java
+++ app/src/main/java/project/study/app/model/dao/StudyAppDatabase.java
@@ -5,10 +5,12 @@ import android.content.Context;
 import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
+import androidx.room.TypeConverters;
 
 import project.study.app.model.domain.QuestionSet;
 
 @Database(entities = {QuestionSet.class}, version = 1, exportSchema = false)
+@TypeConverters({QuestionListConverter.class})
 public abstract class StudyAppDatabase extends RoomDatabase {
 
     private static volatile StudyAppDatabase INSTANCE;
