0889d2a90e7ff31bb2137f8aa24f942eb57b5a4b
5JDR <dellarovere.sandro@gmail.com>
2024-06-26 13:52:58 +0200

    Added test for getEditTextQuestionSetName

--- app/src/androidTest/java/project/study/app/view/ManualModeActivityTest.java
+++ app/src/androidTest/java/project/study/app/view/ManualModeActivityTest.java
@@ -9,9 +9,15 @@ import org.junit.Before;
 import org.junit.After;
 
 import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.replaceText;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+
+import android.widget.EditText;
+
+import static org.junit.Assert.assertEquals;
 
 import project.study.app.R;
 
@@ -37,5 +43,24 @@ public class ManualModeActivityTest {
         onView(withId(R.id.editTextQuestionSetName)).check(matches(isDisplayed()));
         onView(withId(R.id.recyclerView)).check(matches(isDisplayed()));
         onView(withId(R.id.buttonAddQuestionSet)).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void testGetEditTextQuestionSetName() {
+
+        // Set some text in the EditText
+        String inputText = "Sample Question Set Name";
+        onView(withId(R.id.editTextQuestionSetName)).perform(replaceText(inputText));
+
+        // Use ActivityScenario to run the assertion on the activity's main thread
+        scenario.onActivity(activity -> {
+
+            // Verify that getEditTextQuestionSetName returns the correct value
+            EditText editText = activity.getEditTextQuestionSetName();
+            assertEquals(inputText, editText.getText().toString());
+        });
+
+        // Verify that the EditText contains the correct text
+        onView(withId(R.id.editTextQuestionSetName)).check(matches(withText(inputText)));
     }
 }
