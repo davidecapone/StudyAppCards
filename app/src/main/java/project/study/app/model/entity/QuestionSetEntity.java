2db3a0ad8a90a25284a60446b0eadc70a904b726
5JDR <dellarovere.sandro@gmail.com>
2024-06-29 11:03:40 +0200

    Refactored and added contracts to QuestionSetEntity

--- app/src/main/java/project/study/app/model/entity/QuestionSetEntity.java
+++ app/src/main/java/project/study/app/model/entity/QuestionSetEntity.java
@@ -1,11 +1,9 @@
 package project.study.app.model.entity;
 
 import androidx.room.Entity;
-import androidx.room.Ignore;
 import androidx.room.PrimaryKey;
 import androidx.room.TypeConverters;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import project.study.app.model.converters.QuestionListConverter;
@@ -20,41 +18,88 @@ import project.study.app.model.domain.Question;
 @Entity(tableName = "question_sets")
 public class QuestionSetEntity {
 
+    // The primary key for the question set
     @PrimaryKey(autoGenerate = true)
     private int id = 0;
+
+    // The name of the question set
     private String name;
+
+    // The list of questions in the set
     @TypeConverters(QuestionListConverter.class)
     private List<Question> questions;
 
+    /**
+     * Constructor to create a QuestionSetEntity with a name and list of questions.
+     *
+     * @param name The name of the question set.
+     * @param questions The list of questions in the set.
+     */
     public QuestionSetEntity(String name, List<Question> questions) {
+
         this.name = name;
         this.questions = questions;
     }
 
+    /**
+     * Gets the ID of the question set.
+     *
+     * @return The ID of the question set.
+     */
     public int getId() {
         return id;
     }
 
+    /**
+     * Sets the ID of the question set.
+     *
+     * @param id The ID of the question set.
+     */
     public void setId(int id) {
         this.id = id;
     }
 
+    /**
+     * Gets the name of the question set.
+     *
+     * @return The name of the question set.
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Sets the name of the question set.
+     *
+     * @param name The name of the question set.
+     */
     public void setName(String name) {
         this.name = name;
     }
 
+    /**
+     * Gets the list of questions in the set.
+     *
+     * @return The list of questions in the set.
+     */
     public List<Question> getQuestions() {
         return questions;
     }
 
+    /**
+     * Sets the list of questions in the set.
+     *
+     * @param questions The list of questions in the set.
+     */
     public void setQuestions(List<Question> questions) {
         this.questions = questions;
     }
 
+    /**
+     * Adds a question to the set.
+     *
+     * @param question The question to add to the set.
+     */
     public void addQuestion(Question question) {
         this.questions.add(question);
     }
