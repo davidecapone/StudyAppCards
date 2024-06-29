775a4f38ce477de2d4a5426662a6064643cc41a5
5JDR <dellarovere.sandro@gmail.com>
2024-06-29 14:51:48 +0200

    Added contracts to timer class

--- app/src/main/java/project/study/app/util/RealTimer.java
+++ app/src/main/java/project/study/app/util/RealTimer.java
@@ -2,22 +2,44 @@ package project.study.app.util;
 
 import android.os.CountDownTimer;
 
+/**
+ * A real implementation of the Timer interface that uses the CountDownTimer class.
+ */
 public class RealTimer implements Timer {
 
+    // The CountDownTimer object that will be used to implement the timer.
     private CountDownTimer countDownTimer;
 
+    /**
+     * Start the timer with the given duration, interval, and callback.
+     *
+     * @param durationInMillis the total duration of the timer
+     * @param intervalInMillis the interval at which the timer should tick
+     * @param callback the callback to be called when the timer ticks or finishes
+     */
     @Override
     public void start(long durationInMillis, long intervalInMillis, TimerCallback callback) {
+
         if (countDownTimer != null) {
+
             countDownTimer.cancel();
         }
 
         countDownTimer = new CountDownTimer(durationInMillis, intervalInMillis) {
+
+            /**
+             * Called when the timer ticks.
+             *
+             * @param millisUntilFinished The amount of time until finished.
+             */
             @Override
             public void onTick(long millisUntilFinished) {
                 callback.onTick(millisUntilFinished);
             }
 
+            /**
+             * Called when the timer finishes.
+             */
             @Override
             public void onFinish() {
                 callback.onFinish();
--- app/src/main/java/project/study/app/util/TimerCallback.java
+++ app/src/main/java/project/study/app/util/TimerCallback.java
@@ -1,6 +1,19 @@
 package project.study.app.util;
 
+/**
+ * An interface for a callback that can be called when a timer ticks or finishes.
+ */
 public interface TimerCallback {
+
+    /**
+     * Called when the timer ticks.
+     *
+     * @param millisUntilFinished
+     */
     void onTick(long millisUntilFinished);
+
+    /**
+     * Called when the timer finishes.
+     */
     void onFinish();
 }
